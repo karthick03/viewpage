dafea5fd2cc6c5066ac80e0f866304ee05678d1d
faizaanceg <aulisius7@gmail.com>
2015-05-06 15:24:13 +0530

    Integrate with Splash screen

---
+++ app/src/main/java/com/example/karthick/viewpage/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.example.karthick.viewpage;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class SplashActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, 2000);
+    }
+
+}
